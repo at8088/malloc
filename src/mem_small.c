9800dfcdfc8a522ddd7fbc511bcbd7fb35f35000
Anass Tahiri <tahiria@ensipc470.ensimag.fr>
2019-10-09 18:43:57 +0200

    alloc et free small

--- src/mem_small.c
+++ src/mem_small.c
@@ -11,16 +11,26 @@
 void *
 emalloc_small(unsigned long size)
 {
-    /* ecrire votre code ici */
-    void * adresse_allouee_utilisabe = arena.chunkpool;
+ 
+    void * adresse_allouee_utilisable = arena.chunkpool;
     if(arena.chunkpool != NULL){
-        arena.chunkpool = arena.chunkpool + 96 ;                   // supprimer la tete de la liste .
-        adresse_allouee_utilisabe = mark_memarea_and_get_user_ptr(adresse_allouee_utilisabe
+        arena.chunkpool = arena.chunkpool + CHUNKSIZE ;                   // supprimer la tete de la liste .
+        adresse_allouee_utilisable = mark_memarea_and_get_user_ptr(adresse_allouee_utilisable
         ,CHUNKSIZE,SMALL_KIND);
-        return (void *)adresse_allouee_utilisabe;
+        return (void *)adresse_allouee_utilisable;
     }else{
         unsigned long nv_size = mem_realloc_small();
-        
+        void * chunk_suivant = arena.chunkpool + CHUNKSIZE;
+        void ** chunk_courant = arena.chunkpool;
+        while(chunk_suivant < arena.chunkpool + nv_size - CHUNKSIZE){
+            *chunk_courant=(void*)chunk_suivant;
+            chunk_suivant+=CHUNKSIZE;
+            chunk_courant+=CHUNKSIZE/sizeof(void*);
+        }
+        adresse_allouee_utilisable = mark_memarea_and_get_user_ptr(arena.chunkpool
+        ,CHUNKSIZE,SMALL_KIND);
+        arena.chunkpool = arena.chunkpool + CHUNKSIZE ;                  
+        return (void *)adresse_allouee_utilisable;
 
     }
 
@@ -28,5 +38,9 @@ emalloc_small(unsigned long size)
 }
 
 void efree_small(Alloc a) {
-    /* ecrire votre code ici */
+    void * tmp = a.ptr;
+    void ** ptr_tete = (void**)a.ptr;
+    *ptr_tete = (void*)arena.chunkpool;       //ecrire dans la nouvelle tete l'adresse du chunk suivant
+    /*ajout en tete de l'adresse du chunk */
+    arena.chunkpool = tmp;
 }
