2bb812e5f3feb849536129800556cec9d9c88cad
Anass Tahiri <tahiria@ensipc183.ensimag.fr>
2019-10-11 10:36:17 +0200

    fct alloc med ecrite mais pas teestée

--- src/mem_medium.c
+++ src/mem_medium.c
@@ -20,18 +20,54 @@ unsigned int puiss2(unsigned long size) {
 	p++;
     return p;
 }
+void  decoupage_bloc(int indice_case_a_decouper, int nbr_decoupage, unsigned long taille_case_a_decouper){
+    if (nbr_decoupage == 0) return; // condition d'arret est quand on est dans la bonne case
+    nbr_decoupage--; // on décrémente le nombre de découpage pour arriver à la bonne case (cf appel de cette fct)
+    
+    //découper == pointer la case précédente vers la moitié du bloc courant
+    arena.TZL[indice_case_a_decouper-1] = arena.TZL[indice_case_a_decouper] + taille_case_a_decouper/2;
 
+    arena.TZL[indice_case_a_decouper] = NULL;           // NULL == il existe plus de bloc de la taile 2**indice_case_a_decouper
+
+    // écrire l'adresse de l'élément libre suivant (ie le buddy) dans le nouveau bloc créer 
+    // sous forme d'un nombre de 64 bits == sizeof(void*) sur les machines de l'ensimag
+    *((uint64_t*)arena.TZL[indice_case_a_decouper-1]) = (uint64_t)((void*)arena.TZL[indice_case_a_decouper-1] ^ taille_case_a_decouper/2);
+
+    decoupage_bloc(indice_case_a_decouper-1,nbr_decoupage,taille_case_a_decouper/2);
+
+}
 
 void *
 emalloc_medium(unsigned long size)
 {
     assert(size < LARGEALLOC);
     assert(size > SMALLALLOC);
-    unsigned int indice=puiss2(size);
-    if(indice<=TZL_SIZE){
-      return mark_memarea_and_get_user_ptr()
+    int bloc_plus_grand_trouve = 0;
+    int indice = puiss2(size);
+    unsigned long taille_case_i = size/2 ;
+    for(int i = indice ; i< FIRST_ALLOC_MEDIUM_EXPOSANT+arena.medium_next_exponant ; i++){  
+        // on commence la recherche depuis la case de la taille size pour 
+        // trouver le  bloc le + petit qui convient
+        taille_case_i *= 2;                    
+        if(arena.TZL[i]!=NULL){
+            decoupage_bloc(i,i-k,taille_case_i);
+            bloc_plus_grand_trouve = 1;
+            break;
+        }
     }
-    return (void *) 0;
+    // A ce point là soit la case "indice" pointe vers un bloc de taille size soit y avait pas de bloc plus grand disponible
+    if(!bloc_plus_grand_trouve){
+        unsigned long nv_taille_max = mem_realloc_medium();
+        decoupage_bloc(FIRST_ALLOC_MEDIUM_EXPOSANT+arena.medium_next_exponant , 
+        FIRST_ALLOC_MEDIUM_EXPOSANT+arena.medium_next_exponant-k , nv_taille_max); 
+        //On aurait pu utiliser la variable taille_case_i . Elle aura atteint
+        // 2**FIRST_ALLOC_MEDIUM_EXPOSANT+arena.medium_next_exponant
+    }
+
+    void* adresse_a_renvoyer = arena.TZL[indice];
+    arena.TZL[indice] = (void*) (*((uint64_t*)arena.TZL[indice])) ;
+
+    return (void *) adresse_a_renvoyer;
 }
 
 
